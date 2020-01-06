fb961878a355c072d715252dba2b69fc073b836d
Kaitlyn Martinez <kkmartinez95@gmail.com>
2020-01-06 16:54:33 -0500

    VIDEO #9 INSERT SOLUTION

--- BinarySearchTrees.js
+++ BinarySearchTrees.js
@@ -4,26 +4,6 @@ VIDEO #7 Our Tree Classes
 
 */
 
-class BinarySearchTree {
-  constructor() {
-    this.root = null;
-  }
-}
-
-class Node {
-  constructor(value) {
-    (this.value = value), (this.left = null), (this.right = null);
-  }
-}
-
-var tree = new BinarySearchTree();
-tree.root = new Node(15);
-tree.root.right = new Node(20);
-tree.root.left = new Node(10);
-tree.root.left.right = new Node(13);
-
-console.log("TREE >>>>", tree);
-
 /* VIDEO #8 INSERT 
 
 How to insert a node (PSUDOCODE): 
@@ -43,3 +23,62 @@ How to insert a node (PSUDOCODE):
 
 
 */
+
+/* VIDEO #9 INSERT SOLUTION */ // look at line ______ for solution!!
+
+class BinarySearchTree {
+  constructor() {
+    this.root = null;
+  }
+
+  insert(value) {
+    let newNode = new Node(value);
+
+    if (this.root === null) {
+      this.root = newNode;
+      return this;
+    } else {
+      let currentNode = this.root;
+
+      while (currentNode !== null) {
+        if (value === currentNode.value) {
+          return console.log(`The value ${value} is already in this tree!`);
+        }
+        if (value < currentNode.value) {
+          if (currentNode.left === null) {
+            currentNode.left = newNode;
+            return this;
+          }
+          currentNode = currentNode.left;
+        } else if (value > currentNode.value) {
+          if (currentNode.right === null) {
+            currentNode.right = newNode;
+            return this;
+          }
+          currentNode = currentNode.right;
+        }
+      }
+    }
+  }
+}
+
+class Node {
+  constructor(value) {
+    (this.value = value), (this.left = null), (this.right = null);
+  }
+}
+
+var tree = new BinarySearchTree();
+// tree.root = new Node(15);
+// tree.root.right = new Node(20);
+// tree.root.left = new Node(10);
+// tree.root.left.right = new Node(13);
+
+tree.insert(10);
+tree.insert(8);
+tree.insert(3);
+tree.insert(55);
+tree.insert(57);
+tree.insert(10);
+
+console.log("TREE >>>>", tree);
