9cf270a64d78d7523a875c9288fd42e80c78303e
Kaitlyn Martinez <kkmartinez95@gmail.com>
2020-01-06 15:45:45 -0500

    VIDEO #8 Insert method PSUDOCODE

--- BinarySearchTrees.js
+++ BinarySearchTrees.js
@@ -23,3 +23,23 @@ tree.root.left = new Node(10);
 tree.root.left.right = new Node(13);
 
 console.log("TREE >>>>", tree);
+
+/* VIDEO #8 INSERT 
+
+How to insert a node (PSUDOCODE): 
+
+--> Create a new node
+--> Starting at the root
+    --> Check if there is a root, if not - the root now becomes that new node!
+    --> If there IS a root, check if the value of the new node is greater than or less than the value of the root.
+    --> If it is greater
+        + Check to see if there is a node to the right
+            + If there is, move to that node and repeat these steps
+            + If there is not, add that node as the right property
+    --> if it is less
+        + Check to see if there is a node to the left
+            + If there is, move to that node and repeat these steps
+            + If there is not, add that node as the left property
+
+
+*/
