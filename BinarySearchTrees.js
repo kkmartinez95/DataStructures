a79f3451c5c96d427e85c3785fa35562f17acad0
Kaitlyn Martinez <kkmartinez95@gmail.com>
2020-01-07 12:51:30 -0500

    VIDEO #11 FIND  SOLUTION

--- BinarySearchTrees.js
+++ BinarySearchTrees.js
@@ -4,7 +4,7 @@ VIDEO #7 Our Tree Classes
 
 */
 
-/* VIDEO #8 INSERT 
+/* VIDEO #8 & #9 INSERT  --> look at line ___34___ for solution!!
 
 How to insert a node (PSUDOCODE): 
 
@@ -24,7 +24,25 @@ How to insert a node (PSUDOCODE):
 
 */
 
-/* VIDEO #9 INSERT SOLUTION */ // look at line ______ for solution!!
+/* VIDEO #10 & #11 FIND  --> look at line ___84___ for solution!!
+
+How to find a node (PSUDOCODE):
+
+--> Starting at the root
+    --> Check if there is a root, if not - we're done searching!
+    --> If there is a root, check if the value of the new node is the value we are looking for. If we found it, we're done!
+    --> If not, check to see if the value is greater than or less than the value of the root
+    --> If it is greater
+        + Check to see if there is a node to the right
+            + If there is, move to that node and repeat these steps
+            + If there is not, we're done searching!
+    --> If it is less
+        + Check to see if there is a node to the left
+            + If there is, move to that node and repeat these steps
+            + If there is not, we're done searching!
+
+
+*/
 
 class BinarySearchTree {
   constructor() {
@@ -33,32 +51,52 @@ class BinarySearchTree {
 
   insert(value) {
     let newNode = new Node(value);
+    let currentNode = this.root;
 
     if (this.root === null) {
       this.root = newNode;
       return this;
     } else {
-      let currentNode = this.root;
-
       while (currentNode !== null) {
         if (value === currentNode.value) {
-          return console.log(`The value ${value} is already in this tree!`);
+          return console.log(`The value ${value} is already in the BST!`);
         }
-        if (value < currentNode.value) {
-          if (currentNode.left === null) {
-            currentNode.left = newNode;
-            return this;
-          }
-          currentNode = currentNode.left;
-        } else if (value > currentNode.value) {
+
+        if (value > currentNode.value) {
           if (currentNode.right === null) {
             currentNode.right = newNode;
             return this;
+          } else {
+            currentNode = currentNode.right;
           }
-          currentNode = currentNode.right;
+        } else if (value < currentNode.value) {
+          if (currentNode.left === null) {
+            currentNode.left = newNode;
+            return this;
+          } else {
+            currentNode = currentNode.left;
+          }
         }
       }
     }
+  }
+
+  find(value) {
+    if (this.root === null) return false;
+
+    let current = this.root;
+    let found = false;
+
+    while (current && !found) {
+      if (value < current.value) {
+        current = current.left;
+      } else if (value > current.value) {
+        current = current.right;
+      } else {
+        found = true;
+      }
+    }
+    return console.log(`THIS IS THE FOUND VALUE! ${current.value}`);
   }
 }
 
@@ -75,10 +113,43 @@ var tree = new BinarySearchTree();
 // tree.root.left.right = new Node(13);
 
 tree.insert(10);
-tree.insert(8);
-tree.insert(3);
-tree.insert(55);
-tree.insert(57);
-tree.insert(10);
+tree.insert(99);
+tree.insert(109);
+tree.insert(50);
+tree.insert(9);
+tree.insert(1);
+tree.insert(99);
 
-console.log("TREE >>>>", tree);
+tree.find(1);
+
+// console.log("TREE >>>>", tree);
+
+// insert(value) {
+//     let newNode = new Node(value);
+
+//     if (this.root === null) {
+//       this.root = newNode;
+//       return this;
+//     } else {
+//       let currentNode = this.root;
+
+//       while (currentNode !== null) {
+//         if (value === currentNode.value) {
+//           return console.log(`The value ${value} is already in this tree!`);
+//         }
+//         if (value < currentNode.value) {
+//           if (currentNode.left === null) {
+//             currentNode.left = newNode;
+//             return this;
+//           }
+//           currentNode = currentNode.left;
+//         } else if (value > currentNode.value) {
+//           if (currentNode.right === null) {
+//             currentNode.right = newNode;
+//             return this;
+//           }
+//           currentNode = currentNode.right;
+//         }
+//       }
+//     }
+//   }
