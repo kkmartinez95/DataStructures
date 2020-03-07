332b49c34a2f266e09e92d5cf6b48fec9fd22e21
Kaitlyn Martinez <kkmartinez95@gmail.com>
2020-03-07 15:24:05 -0500

    factorial problem

--- Recursion.js
+++ Recursion.js
@@ -6,12 +6,29 @@ function checkForOdd(arr) {
   // check the first elem if it is odd or not
   // if its odd, return false, if its even, shift it off the first elem
   if (arr[0] % 2 === 1) {
-    return console.log("FALSE", arr), false;
+    return console.log("FALSE"), false;
   } else {
     // call on checkforOdd on that shortened array
     arr.shift();
     checkForOdd(arr);
   }
 }
+// TESTED HERE
+// checkForOdd([2, 4, 10, 8, 89797081]);
 
-checkForOdd([2, 4, 10, 8, 89797081]);
+function factorialIter(num) {
+  let total = 1;
+
+  for (let i = num; i > 1; i--) {
+    total *= i;
+  }
+
+  return console.log(total);
+}
+
+function factorial(num) {
+  if (num === 1) return 1;
+  return num * factorial(num - 1);
+}
+
+console.log(factorial(5));
