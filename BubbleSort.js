3263a9b6346e219a010bc4645a581d418886f865
Kaitlyn Martinez <kkmartinez95@gmail.com>
2020-03-08 14:46:42 -0400

    made some optomizations

--- BubbleSort.js
+++ BubbleSort.js
@@ -15,16 +15,26 @@ Pseudo-code:
 */
 
 function bubbleSort(arr) {
-  for (let i = arr.length - 1; i > 0; i--) {
+  let noSwap; // this is to optomize sort
+  //   let passCount = 0; // uncomment this and line 31 to see the difference of optomizing with `noSwap`
+
+  for (let i = arr.length; i > 0; i--) {
+    noSwap = true; // assume no swaps have been made iitially
     for (let j = 0; j < i - 1; j++) {
+      console.log("here", arr);
       if (arr[j] > arr[j + 1]) {
         let temp = arr[j];
         arr[j] = arr[j + 1];
         arr[j + 1] = temp;
+        noSwap = false; // once we have made a swap, toggle the value
       }
+      //   passCount++;
     }
+    if (noSwap) break; // if the no swap has been made, no need to continue the loop again
   }
-  return console.log(arr);
+  return console.log("final", arr, passCount);
 }
 
-bubbleSort([33, 2, 1, 5, 66, 4, 200]);
+// TESTED HERE
+bubbleSort([8, 1, 2, 3, 4, 5, 6, 7]); // OPTOMIZED TEST
+// bubbleSort([33, 2, 1, 5, 66, 4, 200]);
